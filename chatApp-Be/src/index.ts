0a1fbcca75d4e22d6866e76bb6a205bfa82b4053
gana6547 <ganeshbobade6547@gmail.com>
2025-04-01 16:02:40 +0530

    edited path

--- chatApp-Be/src/index.ts
+++ chatApp-Be/src/index.ts
@@ -25,14 +25,20 @@ app.use(express.json());
 app.use("/api/auth",auhtRouter)
 app.use("/api/messages",messageRouter)
 
+const __dirname1=path.resolve();
+
 if(process.env.NODE_ENV==="production"){
-   app.use(express.static(path.join(__dirname,"../chat-App-Fe/dist")))
+   app.use(express.static(path.join(__dirname1,"../chat-App-Fe/dist")))
 
 
    app.get("*",(req,res)=>{
-    res.sendFile(path.join(__dirname,"chat-App-Fe","dist","index.html"));
+    res.sendFile(path.resolve(__dirname1,"chat-App-Fe","dist","index.html"));
    })
-}
+}else {
+    app.get("/", (req, res) => {
+      res.send("API is running..");
+    });
+  }
 
 
 app.get("/",(req,res)=>{
